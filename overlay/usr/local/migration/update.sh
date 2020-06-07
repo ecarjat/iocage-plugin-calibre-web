60215899f4b22d28caad07151e02f1545a75d93e
Albert Serrallé Ríos <aserrallerios@gmail.com>
2020-06-07 10:03:26 +0200

    Improve migration

--- overlay/usr/local/migration/update.sh
+++ overlay/usr/local/migration/update.sh
@@ -3,6 +3,8 @@
 oldcfgdir="/usr/local/app/calibre-web"
 cfgdir="/usr/local/etc/calibre-web"
 
-[ ! -d "$cfgdir" ] && mkdir -p $cfgdir && chown calibreweb:calibreweb $cfgdir/app.db
-[ -f "$oldcfgdir/app.db" ] && mv $oldcfgdir/app.db $cfgdir && chown calibreweb:calibreweb $cfgdir/app.db
-[ -f "$oldcfgdir/gdrive.db" ] && mv $oldcfgdir/gdrive.db $cfgdir && chown calibreweb:calibreweb $cfgdir/gdrive.db
+[ ! -d "$cfgdir" ] && mkdir -p $cfgdir
+[ -f "$oldcfgdir/app.db" ] && mv $oldcfgdir/app.db $cfgdir
+[ -f "$oldcfgdir/gdrive.db" ] && mv $oldcfgdir/gdrive.db $cfgdir
+
+chown -R calibreweb:calibreweb $cfgdir
